bc5bd44fdc192bbe236aeb28103ebe2f75575578
AkiraHonda <05410kira@gmail.com>
2022-02-13 22:56:43 +0900

    Typography

--- src/components/MaterialUI.tsx
+++ src/components/MaterialUI.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Button, makeStyles } from "@material-ui/core";
+import { Button, makeStyles, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles({
   btnStyle: {
     background: "green",
     padding: "3px 50px",
+  },
+  typoStyle: {
+    color: "blue",
   },
 });
 
@@ -12,6 +15,15 @@ const MaterialUI: React.FC = () => {
   const classes = useStyles();
   return (
     <div>
+      <Typography
+      className={classes.typoStyle}
+       color="secondary"
+       variant="h1"
+       align="left"
+       gutterBottom 
+       noWrap
+      >
+        Hello UIHello UIHello UIHello UIHello</Typography>
       <Button className={classes.btnStyle} variant="contained" color="primary">TEST BUTTON</Button>
     </div>
   )
